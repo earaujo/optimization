d869a85eabb2471ccec8b34f941847954ecf99fc
Ewerton Araujo <earaujoassis@gmail.com>
2013-07-08 21:37:33 -0300

    Copyright update

--- src/lp/lp-reader.h
+++ src/lp/lp-reader.h
@@ -1,4 +1,4 @@
-// Copyright 2011 Éwerton Assis
+// Copyright 2011 Ewerton Assis and Juliane Oliveira
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
